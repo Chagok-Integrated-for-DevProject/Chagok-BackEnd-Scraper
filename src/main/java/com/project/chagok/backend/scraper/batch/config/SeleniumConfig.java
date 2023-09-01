951da1e120a5fdc7fe284bb7975c44721ab933ff
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2023-09-01 17:59:19 +0900

    selenium 드라이버 다운로드 방식 추가
    jib config 추가

--- src/main/java/com/project/chagok/backend/scraper/batch/config/SeleniumConfig.java
+++ src/main/java/com/project/chagok/backend/scraper/batch/config/SeleniumConfig.java
@@ -1,8 +1,12 @@
 package com.project.chagok.backend.scraper.batch.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +14,14 @@ import org.springframework.context.annotation.Configuration;
 public class SeleniumConfig {
 
     @Bean
-    public WebDriver chromeWebDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
+    public WebDriver edgeWebDriver() {
+        // chrome driver setup
+        WebDriverManager.chromedriver().setup();
 
-        return new ChromeDriver(options);
+        // chrom driver options
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+
+        return new ChromeDriver(chromeOptions);
     }
 }
