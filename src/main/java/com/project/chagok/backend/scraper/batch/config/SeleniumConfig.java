558d27d43d0b37eb0732d49af57077bc61b899a2
DEREVGEN <96948245+DEREVGEN@users.noreply.github.com>
2023-09-02 20:01:59 +0900

    jib image 수정
    test프로퍼티 수정

--- src/main/java/com/project/chagok/backend/scraper/batch/config/SeleniumConfig.java
+++ src/main/java/com/project/chagok/backend/scraper/batch/config/SeleniumConfig.java
@@ -4,9 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chromium.ChromiumOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class SeleniumConfig {
 
     @Bean
-    public WebDriver edgeWebDriver() {
+    public WebDriver chromeWebDriver() {
         // chrome driver setup
         WebDriverManager.chromedriver().setup();
 
